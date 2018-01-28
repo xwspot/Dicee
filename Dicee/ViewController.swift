992e2b69d0066fd918764f806c3f50d102f976e7
xwspot <perfect.1988@hotmail.com>
2018-01-28 19:29:50 +0800

    Update Dice Images with Roll Button Pressed.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceNo1: Int = 0
+    var randomDiceNo2: Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceNo1 = Int(arc4random_uniform(6))
+        randomDiceNo2 = Int(arc4random_uniform(6))
+        diceImageView1.image = UIImage(named: diceArray[randomDiceNo1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceNo2])
+    }
+    
 }
 
